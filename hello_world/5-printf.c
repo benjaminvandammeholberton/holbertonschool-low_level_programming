bbb2aa879293d02a35f5e346bee855ab9a75544a
benjaminvandammeholberton <you@example.com>
2023-02-17 00:46:02 -0800

    5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,9 @@
 # include <stdio.h>
 
 /**
- * main - Entry point
- * 
- * Description: 'this program is printing : with proper grammar, but the outcome is a piece of art,'
- * 
+ * main - Entry point 
+ * Description: 'this program is printing :
+ * with proper grammar, but the outcome is a piece of art,'
  * Return: Always 0 (Success)
  */
 
