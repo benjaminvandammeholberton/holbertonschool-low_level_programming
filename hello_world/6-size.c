477e1932864948e722da60eb9f8df5e5fa98efcc
benjaminvandammeholberton <you@example.com>
2023-02-17 03:47:48 -0800

    6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,12 +7,12 @@
  * Return : Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
-printf("Size of a char: %d\n", (int)sizeof(char));
-printf("Size of a int: %d\n", (int)sizeof(int));
-printf("Size of a long int: %d\n", (int)sizeof(long int));
-printf("Size of a long long int: %d\n", (int)sizeof(long long int));
-printf("Size of a float:%d\n", (int)sizeof(float));
+printf("Size of a char: %d byte(s)\n", (int)sizeof(char));
+printf("Size of a int: %d byte(s)\n", (int)sizeof(int));
+printf("Size of a long int: %d byte(s)\n", (int)sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", (int)sizeof(long long int));
+printf("Size of a float:%d byte(s)\n", (int)sizeof(float));
 return (0);
 }
