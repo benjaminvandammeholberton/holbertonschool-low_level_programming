58bb5209d3fe1dfaca916cbfcdc03d038ba90478
benjaminvandammeholberton <you@example.com>
2023-02-17 00:29:30 -0800

    5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,11 @@
 # include <stdio.h>
-
 /**
- * This fonction is printing : "with proper grammar, but the outcome is a piece of art,"
+ * main - Entry point
+ *
+ * Return : Always 0 (Success)
  */
-
 int main(void)
 {
-printf ("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
