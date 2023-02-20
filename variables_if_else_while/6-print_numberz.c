b536e8aceb40afd7cd8c31053c13ba8c0fe081fb
benjaminvandammeholberton <you@example.com>
2023-02-20 05:44:09 -0800

    6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -2,11 +2,12 @@
 /**
  * main - Entry Point
  *
- * Return : 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
+
 	for (n = 48 ; n <= 57 ; n++)
 		putchar(n);
 	putchar('\n');
