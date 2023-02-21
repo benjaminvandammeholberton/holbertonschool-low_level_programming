ee6e12c13a59597b8fc03eaf92fd2f17072caeec
benjaminvandammeholberton <you@example.com>
2023-02-21 04:57:57 -0800

    6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 /**
- *
- *
- *
+ * _abs- computes the absolute value of an integer.
+ *@c: number to check
+ *Return: c for positive numbers and 0 or (c - 2 * c) for negative numbers
  */
 
 int _abs(int c)
@@ -9,5 +9,5 @@ int _abs(int c)
 	if (c >= 0)
 		return (c);
 	else
-		return (c - 2*c);
+		return (c - 2 * c);
 }
