b8eac53af7e952aedce16a96203c6777260c2991
benjaminvandammeholberton <you@example.com>
2023-02-22 04:38:51 -0800

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *add - beginning of program
+ *
+ *Description: prints the sum of two numbers
+ *
+ *@a:first number of the sum
+ *@b:second number of the sum
+ *
+ *Return: the sum of the numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
