832e632f1afdbd22e19220bc4aa10b2e2a245356
benjaminvandammeholberton <you@example.com>
2023-02-23 09:11:34 -0800

    9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,7 @@ int main(void)
 {
 	int n;
 
-	for (n = 1 ; n <= 100 ; n++)
+	for (n = 1 ; n <= 99 ; n++)
 	{
 		if ((n % 3) == 0 && (n % 5) == 0)
 			printf("FizzBuzz ");
@@ -25,6 +25,7 @@ int main(void)
 		else
 			printf("%d ", n);
 	}
+	printf("Buzz");
 	printf("\n");
 	return (0);
 }
