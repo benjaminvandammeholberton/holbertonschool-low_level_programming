462a2f533f1268bc7ae18c50ab65ef96dbfa22df
benjaminvandammeholberton <you@example.com>
2023-03-01 02:30:28 -0800

    7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+/**
+ * leet - function that encodes a string into 1337.
+ * @s: the string to encode
+ * Return: the string encoded
+ */
+
+char *leet(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char a[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char b[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	for (i = 0 ; s[i] != '0' ; i++)
+	{
+		for (j = 0 ; j < 10 ; j++)
+		{
+			if (s[i] == a[j])
+				s[i] = b[j];
+		}
+	}
+	return (s);
+}
