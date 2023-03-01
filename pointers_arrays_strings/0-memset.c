4ddea0f8a4c463cfb1b8e99c61b00c0715ebd717
benjaminvandammeholberton <you@example.com>
2023-03-01 03:28:04 -0800

    0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+/**
+ * *_memset - function that fills memory with a constant byte.
+ * @s: the string to modifie
+ * @n: he number of bytes to be set to the value.
+ * @b: the value to be set.
+ * Return: the string modified
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0 ; i <= n ; i++)
+		s[i] = b;
+	return (s);
+}
