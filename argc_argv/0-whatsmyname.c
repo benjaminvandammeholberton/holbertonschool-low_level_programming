9a88a75f92d2f651fdd4e9068f153f796d8c556f
benjaminvandammeholberton <you@example.com>
2023-03-07 00:46:18 -0800

    0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,13 +2,14 @@
 #include "main.h"
 /**
  * main - function that prints its name, followed by a new line.
- *
- * RETURN: Always 0 (success)
+ * @argc: arguement count
+ * @argv: argument value, a strings that come after calling function
+ * Return: Always 0 (success)
  */
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
+	if (argc > 0)
 	printf("%s\n", argv[0]);
-	(void)argc;
 	return (0);
 }
