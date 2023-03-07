d40bcb4593b40f97222a9e418f6a5a9f4902119a
benjaminvandammeholberton <you@example.com>
2023-03-07 02:56:57 -0800

    3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * main - program that multiplies two numbers.
- * @argc: arguement count
- * @argv: argument value, a strings that come after calling function
- * Return: Always 0 (success)
+ * main - A program that multiplies two numbers
+ * @argc: The arguments' counter
+ * @argv: The arguments' values
+ * Return: Always 0 (Success)
  */
-
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	if (argc < 2)
-		printf("Error");
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	return (0);
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+		printf("%d\n", result);
+		return (0);
+	}
 }
