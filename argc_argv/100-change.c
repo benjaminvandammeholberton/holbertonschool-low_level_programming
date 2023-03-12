683e109bda7b9fb57ada47f45c71ca591308a847
Benjamin Vandamme <benjamin@MacBook-Air.local>
2023-03-12 09:55:54 +0100

    100

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -17,16 +17,15 @@ int r = 0;
 	
 if (argc != 2)
 {	
-	printf("Error");
+	printf("Error\n");
 	return (1);
 }
-      
 
 c = n / 25;
 r = n % 25;
 if (r == 0)
 {
-printf("%d", c);	
+printf("%d\n", c);	
 return (c);
 }
 
@@ -35,7 +34,7 @@ c = c + r / 10;
 r = r % 10;
 if (r == 0)
 {
-printf("%d", c);
+printf("%d\n", c);
 return (c);
 }
 
@@ -45,7 +44,7 @@ c = c + r / 5;
 r = r % 5;
 if (r == 0)
 {
-printf("%d", c);
+printf("%d\n", c);
 return (c);
 }
 
@@ -53,14 +52,14 @@ c = c + r / 2;
 r = r % 2;
 if (r == 0)
 {
-printf("%d", c);
+printf("%d\n", c);
 return (c);
 }
 
 c = c + r;
 
 
-printf("%d", c);
+printf("%d\n", c);
 
 return (c);
 }
