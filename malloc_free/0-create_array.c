c3c6ff70d4b170999737ca01ca93413779f5af85
Benjamin Vandamme <benjamin@pl118833.ag1313.nttpc.ne.jp>
2023-03-13 11:23:09 +0100

    0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -19,10 +19,10 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 
 	array = malloc(sizeof(char) * size);
+	if (array == NULL)
+                return (NULL);
 	array[0] = c;
 	for (i = 0 ; i <= size ; i++)
 		array[i] = array[0];
-	if (array == NULL)
-		return (NULL);
 	return (array);
 }
