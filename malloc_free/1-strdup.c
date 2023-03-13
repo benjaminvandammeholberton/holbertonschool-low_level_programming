53d33398b2855bf2616a3c550841cd0d30d58c71
Benjamin Vandamme <benjamin@pl118833.ag1313.nttpc.ne.jp>
2023-03-13 13:14:09 +0100

    1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -16,12 +16,14 @@ char *_strdup(char *str)
 	char *strDuplicate;
 	int i, j, l = 0;
 
+	if (str == NULL)
+		return (NULL);
 	for (i = 0 ; str[i] != 0 ; i++)
 		l = l + 1;
 	if (str == NULL)
 		return (NULL);
 
-	strDuplicate = malloc(sizeof(char) * l);
+	strDuplicate = malloc(sizeof(char) * l+1);
 	if (strDuplicate == NULL)
 		return (NULL);
 	for (j = 0; str[j] != 0; j++)
