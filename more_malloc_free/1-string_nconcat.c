546c01a1887af7389a1d69ba9ae7e4773f53d112
Benjamin Vandamme <benjamin@MacBook-Air.local>
2023-03-15 10:41:31 +0100

    1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -25,8 +25,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	lenght = l1 + n;
 
-	printf("%d\n", lenght);
-
 	concat = (char *) malloc(sizeof(*concat * (lenght + 1)));
 	if (concat == NULL)
 		return (NULL);
@@ -34,7 +32,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < l1; i++)
 		concat[i] = s1[i];
 
-	printf("%u\n", n);
 	for (i = 0; i < n ; i++)
 		concat[i + l1] = s2[i];
 
