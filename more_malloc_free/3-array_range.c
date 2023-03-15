1952c61470c13f1e2a3cdd69162b7fe2ddbbb028
Benjamin Vandamme <benjamin@MacBook-Air.local>
2023-03-15 13:36:59 +0100

    3

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -23,7 +23,7 @@ int *array_range(int min, int max)
 	if (array == NULL)
 		return (NULL);
 
-	for (i = 0 ; i < range ; i++)
+	for (i = 0 ; i <= range ; i++)
 	array[i] = min + i;
 
 	return (array);
