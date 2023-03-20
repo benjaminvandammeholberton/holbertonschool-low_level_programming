afc82d5e4316f4a568ec59e6b323c5bcc9fa1df6
Benjamin Vandamme <benjamin@pl118833.ag1313.nttpc.ne.jp>
2023-03-20 10:38:37 +0100

    0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ *
+ * Description: This structure is about information about dog.
+ */
+
+struct dog {
+    char *name;
+    float age;
+    char *owner;
+};
