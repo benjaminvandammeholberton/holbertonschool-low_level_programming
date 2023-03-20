0648f204705b4f7e957024db66b42c1d815a0978
Benjamin Vandamme <benjamin@MacBook-Air.local>
2023-03-20 18:37:24 +0100

    2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -16,8 +16,7 @@ void print_dog(struct dog *d)
 	else
 		printf("Name: %s\n", d->name);
 
-	else
-		printf("Age: %f\n", d->age);
+	printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
