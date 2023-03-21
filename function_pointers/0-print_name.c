39b50317dd84b81f076ba59767aa1a76933597bb
Benjamin Vandamme <benjamin@MacBook-Air.local>
2023-03-21 11:00:04 +0100

    0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+  * print_name - Function that prints a name
+  *
+  * @name: name to print
+  * @f: function that print the name
+  *
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
