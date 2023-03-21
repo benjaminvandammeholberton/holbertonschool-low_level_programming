62684a676591ba69a7beeccb318b25f82bdab2da
Benjamin Vandamme <benjamin@MacBook-Air.local>
2023-03-21 09:23:41 +0100

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,20 +9,36 @@
  * @age: age's dog
  * @owner: owner's dog
  *
- * Return: pointe on the new structure of the new dog
+ * Return: pointer on the new structure of the new dog
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	char *namecpy, *ownercpy;
+	int lenghtname = 0, lenghtowner = 0, i;
 
+	for (i = 0; name[i] != 0; i++)
+		lenghtname++;
+	for (i = 0; owner[i] != 0; i++)
+		lenghtowner++;
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-
-	new_dog->name = name;
+	namecpy = malloc(lenghtname + 1);
+	if (namecpy == NULL)
+		return (NULL);
+	for (i = 0; i < lenghtname + 1; i++)
+		namecpy[i] = name[i];
+	namecpy[i] = 0;
+	ownercpy = malloc(lenghtowner + 1);
+	if (ownercpy == NULL)
+		return (NULL);
+	for (i = 0; ownercpy[i] < lenghtowner + 1; i++)
+		ownercpy[i] = owner[i];
+	ownercpy[i] = 0;
+	new_dog->name = namecpy;
 	new_dog->age = age;
-	new_dog->owner = owner;
-
+	new_dog->owner = ownercpy;
 	return (new_dog);
 }
