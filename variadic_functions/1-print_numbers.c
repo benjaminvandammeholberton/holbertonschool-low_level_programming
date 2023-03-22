d591722a8948b523203ec1ae9125b46e27da9dec
Benjamin Vandamme <benjamin@pl118833.ag1313.nttpc.ne.jp>
2023-03-22 15:43:19 +0100

    1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -15,7 +15,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(ap, n);
 
-	if (separator == NULL)
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	else if (separator == NULL)
 		for (i = 0; i < n - 1; i++)
 			printf("%d", va_arg(ap, int));
 
