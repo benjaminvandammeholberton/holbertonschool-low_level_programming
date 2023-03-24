bd2eacadcce91eeeb02fd58a0a53fa0c2f3b855a
Benjamin Vandamme <benjamin@MacBook-Air.local>
2023-03-24 19:59:28 +0100

    3

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,26 +2,23 @@
 #define VARIADI_FUNCTIONS_H
 
 #include <stdarg.h>
+
 /**
- * struct structure - Array structure
- *
- * @letter: letter associated to the parameter passed
- * @f: The function that print the parameter
+ * struct printer - A new struct type defining a printer.
+ * @symbol: A symbol representing a data type.
+ * @print: A function pointer to a function that prints
+ *         a data type corresponding to symbol.
  */
-typedef struct structure
+typedef struct printer
 {
-	char *letter;
-	void (*f)(va_list arg);
-} structure;
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printer_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void printInt(va_list ap);
-void printChar(va_list ap);
-void printFloat(va_list ap);
-void printString(va_list ap);
-
-
 
 #endif
