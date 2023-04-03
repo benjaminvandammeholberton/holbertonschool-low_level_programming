5d11e5b1caba33b1cb2b9b08b291bb892a952ed5
Ben <holberton@MacBook-Air.local>
2023-04-03 15:48:12 +0200

    4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - ffunction that frees a list
+ * @head: first element of the list
+ */
+
+void free_list(list_t *head)
+{
+	list_t tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
