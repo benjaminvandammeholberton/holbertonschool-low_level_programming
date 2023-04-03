da0ccebf75afbf8e35d478bee639e9783ecbc08e
Ben <holberton@MacBook-Air.local>
2023-04-03 21:31:23 +0200

    100

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@ __attribute__((constructor))
 
 void before_main(void)
 {
-	printf("s You're beat! and yet, you must allow,\n\
-I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
