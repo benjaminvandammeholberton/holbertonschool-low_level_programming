d1c47f92ae2106763886818b19a5a01fceaba6a7
Ben <holberton@MacBook-Air.local>
2023-04-04 20:04:34 +0200

    2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -19,7 +19,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new->n = n;
 	new->prev = NULL;
-	new->next = *head;
+	new->next = NULL;
+
+	if (*head != NULL)
+		(*head)->prev = new;
 
 	*head = new;
 	return (*head);
