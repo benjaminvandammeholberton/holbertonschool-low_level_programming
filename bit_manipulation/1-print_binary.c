9ec18fa4fbd5247a45c8788cd3ca56ab26c04c27
Ben <holberton@MacBook-Air.local>
2023-04-05 21:33:55 +0200

    1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_binary - function that prints the binary representation of a number
+ * @n: number that should be printed
+ */
+
+void print_binary(unsigned long int n)
+{
+	int mask = 32768;
+	int flag = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (mask > 0)
+	{
+		if ((n & mask) == 0 && flag == 1)
+			_putchar('0');
+		else if ((n & mask) != 0)
+		{
+			_putchar('1');
+			flag = 1;
+		}
+		mask = mask >> 1;
+	}
+}
