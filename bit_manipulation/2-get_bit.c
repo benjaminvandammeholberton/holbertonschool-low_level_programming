5d61406e08d638a5180a4f713f9d5550091d56f5
Ben <holberton@pl118833.ag1313.nttpc.ne.jp>
2023-04-06 09:33:40 +0200

    2

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_bit - function that returns the value of a bit at a given index
+ * @n: number checked
+ * @index: index of the bit wanted to get
+ * Return: the value of the bit at index or -1 if an error occured
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bitStatus;
+
+	bitStatus = (n >> index) & 1;
+
+	return (bitStatus);
+}
