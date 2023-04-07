15ba52d9187794f88c68abd28c768b43d7ec18c2
Ben <holberton@MacBook-Air.local>
2023-04-07 13:38:29 +0200

    5

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - function that returns the number of bits needed to flip
+ * from one number to another
+ * @n: first number to compare
+ * @m: second number to compare
+ * Return: The number of bits to flip
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int temp;
+	int i;
+	int count = 0;
+
+	temp = (n ^ m);
+
+	for (i = 0; i < 32; i++)
+	{
+		if ((temp & (1 << i)) != 0)
+			count++;
+	}
+	return (count);
+}
