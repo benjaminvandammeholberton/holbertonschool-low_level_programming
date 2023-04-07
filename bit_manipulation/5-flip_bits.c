e8a115390faf951c1c9b4491f972551a4b9e3adf
Ben <holberton@MacBook-Air.local>
2023-04-07 13:39:25 +0200

    5

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -16,7 +16,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	temp = (n ^ m);
 
-	for (i = 0; i < 32; i++)
+	for (i = 0; i < 64; i++)
 	{
 		if ((temp & (1 << i)) != 0)
 			count++;
