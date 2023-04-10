47c75abeb6454d21534848ea62cc51422ae43f36
Ben <holberton@MacBook-Air.local>
2023-04-10 12:49:28 +0200

    0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - function that reads a text file and
+ * prints it to the POSIX standard output
+ *
+ * @filename: the name of the file that has to be print
+ * @letters: the number of letters it should read and print
+ *
+ * Return: the actual number of letters it could read and print
+ * 0, if the file can not be opened or read or
+ * if write fails or does not write the expected amount of bytes
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	size_t i;
+	char *buff;
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	read(fd, buff, letters);
+	for (i = 0; i < letters; i++)
+		putchar(buff[i]);
+
+	return (letters);
+}
