245e46135a74dd25c3b313214823601bdb0b3e71
Ben <holberton@MacBook-Air.local>
2023-04-11 10:37:49 +0200

    1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,10 +17,11 @@ int create_file(const char *filename, char *text_content)
 	char *buf;
 	size_t count;
 
-	if (text_content == NULL)
-		return (-1);
-	buf = text_content;
-	count = strlen(buf);
+	if (text_content != NULL)
+	{
+		buf = text_content;
+		count = strlen(buf);
+	}
 
 	fd = creat(filename, S_IRUSR | S_IWUSR);
 	if (fd == -1)
