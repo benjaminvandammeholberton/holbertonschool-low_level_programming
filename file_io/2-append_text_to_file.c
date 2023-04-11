65b0176cfe29bf2da7d3e09a00fece341d89494b
Ben <holberton@MacBook-Air.local>
2023-04-11 12:51:24 +0200

    2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -20,8 +20,11 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	buf = text_content;
-	count = strlen(buf);
+	if (text_content != NULL)
+	{
+		buf = text_content;
+		count = strlen(buf);
+	}
 
 	fd = open(filename, O_RDWR | O_APPEND);
 	if (fd == -1)
