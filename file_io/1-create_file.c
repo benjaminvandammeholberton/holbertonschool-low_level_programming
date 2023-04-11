a4f09a0548a56010fb4166fc45655b0071d53bed
Ben <holberton@MacBook-Air.local>
2023-04-11 10:27:27 +0200

    1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+
+/**
+ * create_file - function that creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	char *buf;
+	size_t count;
+
+	buf = text_content;
+	count = strlen(buf);
+
+	fd = creat(filename, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	write(fd, buf, count);
+
+	return (1);
+}
