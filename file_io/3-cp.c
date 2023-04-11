9a4686c5a7f272c477c5bd40d858348844bb21dc
Ben <holberton@MacBook-Air.local>
2023-04-11 17:00:45 +0200

    3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -17,6 +17,7 @@ int main (int argc, char **argv)
 	int fd1;
 	int fd2;
 	char *buf;
+	int a = 1;
 
 	buf = malloc(1024);
 	if (buf == NULL)
@@ -29,15 +30,19 @@ int main (int argc, char **argv)
 	}
 
 	file_from = open(argv[1], S_IRUSR | S_IWUSR);
+	file_to = creat(argv[2], S_IRUSR | S_IWUSR);
 	if (file_from == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	read(file_from, buf, 1024);
 
-	file_to = creat(argv[2], S_IRUSR | S_IWUSR);
+	while (a != 0)
+	{	
 	write(file_to, buf, 1024);
+	a = read(file_from, buf, 1024);
+	
+	}
 	if (file_to == -1)
 		{
 			dprintf(2, "Error: Can't write to %s\n", argv[2]);
@@ -57,6 +62,6 @@ int main (int argc, char **argv)
 		dprintf(2, "Error: Can't close fd %d", fd2);
 		exit (100);
 	}
-
+	free(buf);
 	return (0);
 }
