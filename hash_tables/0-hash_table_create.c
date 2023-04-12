743857372df419015ade51afd73e5688f9240b31
Ben <holberton@MacBook-Air.local>
2023-04-12 17:14:46 +0200

    0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -7,16 +7,16 @@
  *
  * @size: size of the array
  *
- * Return: A pointer to tje newly created hash table
+ * Return: A pointer to the newly created hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
+	hash_table_t **hash_table;
 
-	hash_table = malloc(sizeof(hash_table_t) * size);
-	if (hash_table == NULL)
-        return (NULL);
+	*hash_table = malloc(sizeof(hash_table_t) * size);
+	if (*hash_table == NULL)
+		return (NULL);
 
-	return (hash_table);
+	return (*hash_table);
 }
