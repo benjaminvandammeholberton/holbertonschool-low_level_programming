84599e353e3e17f773cede4529426ffc52638ac7
Ben <holberton@MacBook-Air.local>
2023-04-13 09:28:03 +0200

    2

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -10,10 +10,9 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int key_index;
+	unsigned long int key_index;
 
-    key_index = hash_djb2(key) % size;
+	key_index = hash_djb2(key) % size;
 
-
-    return (key_index);
+	return (key_index);
 }
