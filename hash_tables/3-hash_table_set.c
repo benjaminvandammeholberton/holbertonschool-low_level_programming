64799c65f9f09c5864b45198389ec46b1c7b9dbb
Ben <holberton@MacBook-Air.local>
2023-04-14 13:36:20 +0200

    3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -19,6 +19,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	unsigned long int size = ht->size;
 
+	if (ht == NULL)
+		return(0);
+
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
