99f406fe7261438c47f33c3a41f1a007d0e62132
Ben <holberton@MacBook-Air.local>
2023-04-14 16:37:52 +0200

    5

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -9,13 +9,19 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
+	int flag = 0;
 
+	printf("{");
 	for (i = 0; i < ht->size; i++)
 		if (ht->array[i] == NULL)
 			continue;
 		else
-		{		
-				printf("%s : %s", ht->array[i]->key, ht->array[i]->value);
+		{
+				if (flag == 1)
+					printf(", ");
+				printf("'%s' : '%s'", ht->array[i]->key, ht->array[i]->value);
+				flag = 1;
 				ht->array[i] = ht->array[i]->next;
-		}			
+		}
+	printf("}\n");
 }
