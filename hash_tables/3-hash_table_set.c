c53c3b130354a1a03c657fa798033fc9ee132ba9
Ben <holberton@MacBook-Air.local>
2023-04-14 13:20:16 +0200

    3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -20,9 +20,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	unsigned long int size = ht->size;
 
-	printf("dupkey----> %s\n", dupkey);
-	printf("dupvalue----> %s\n", dupvalue);
-
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
@@ -32,14 +29,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->next = NULL;
 
 	index =  key_index((unsigned char *)dupkey, size);
-	printf("index : %lu\n", index);
 	if (ht->array[index] == NULL)
 	{
 		ht->array[index] = new_node;
 		return (1);
 	}
 	tmp_node = ht->array[index];
-	printf("tmp->key %s\n", tmp_node->key);
 	while (tmp_node->next != NULL)
 		tmp_node = tmp_node->next;
 	tmp_node->next = new_node;
